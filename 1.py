a8a168922f3369b9f81d5a54ec6b96000f598d9c
GaRIk696 <vikingkadet21@yandex.ru>
2025-04-28 17:15:48 +0300

    upd

--- 1.py
+++ 1.py
@@ -1,20 +1,17 @@
-def stepen():
-    i = input("Введите числа через пробел: ")
-    numbers = i.split()
-    power = int(input("Введите степень: "))
-    result = []
-    for item in numbers:
-        try:
-            num = float(item)
+i = input("Введите числа через пробела: ")
+numbers = i.split()
+power = int(input("Введите степень: "))
+result = []
+for item in numbers:
+    try:
+        num = float(item)
 
-            if num.is_integer():
-                num = int(num)
+        if num.is_integer():
+            num = int(num)
 
-            powered = num ** power
-            result.append(str(powered))
-        except ValueError:
+        powered = num ** power
+        result.append(str(powered))
+    except ValueError:
 
-            result.append(item * power)
-    print("Вывод:", " ".join(result))
-
-stepen()
+        result.append(item * power)
+print("Вывод:", " ".join(result))
